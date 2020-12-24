b8065a7716beaf7fc5fe83c5fc25061410b42145
frpericacho <frpericacho@gmail.com>
2020-12-25 00:22:58 +0100

    formateado

--- Section10/script.js
+++ Section10/script.js
@@ -1,3 +1,3 @@
-var userName = prompt("cual es tu nombre");
+var userName = prompt("cual es tu Name");
 var userAge = prompt("cual es tu edad");
 alert(userName + " has vivido: " + userAge * 365 + " dias");
